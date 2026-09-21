0858203251d674b7d86a5e87dd4d82b67f5a3046
amna <amnaa.qader114@gmail.com>
2026-09-21 20:15:49 +0300

    MVS created

--- src/main/java/com/ga/movies/models/MoviesModel.java
+++ src/main/java/com/ga/movies/models/MoviesModel.java
@@ -3,9 +3,10 @@ package com.ga.movies.models;
 import java.util.ArrayList;
 
 public class MoviesModel {
-    private String applicationName;
-    private String name;
-    private String description;
-    private String theme;
-    private ArrayList<String> movies;
+
+
+    public MoviesModel(){
+
+    }
+
 }
---
+++ src/main/java/com/ga/movies/models/ProfileModel.java
@@ -0,0 +1,8 @@
+package com.ga.movies.models;
+
+public class ProfileModel {
+    private String name;
+    private String intro;
+    private String theme;
+
+}
