f84217b7a4cb1cc0fd86b00631bd233522f47c0a
amna <amnaa.qader114@gmail.com>
2026-09-21 19:43:20 +0300

    first commit

---
+++ src/main/java/com/ga/movies/controller/MoviesController.java
@@ -0,0 +1,4 @@
+package com.ga.movies.controller;
+
+public class MoviesController {
+}
---
+++ src/main/java/com/ga/movies/models/MoviesModel.java
@@ -0,0 +1,11 @@
+package com.ga.movies.models;
+
+import java.util.ArrayList;
+
+public class MoviesModel {
+    private String applicationName;
+    private String name;
+    private String description;
+    private String theme;
+    private ArrayList<String> movies;
+}
